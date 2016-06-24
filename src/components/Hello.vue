1a2913fe3c75984d32b06458c58113dfb6b40164
wowhy <wowhy@outlook.com>
2016-06-24 16:28:10 +0800

    修改eslint

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,21 +6,21 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       // note: changing this line won't causes changes
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'Hello World!'
-    }
-  }
-}
+      msg: 'Hello World!',
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  color: #42b983;
-}
+  h1 {
+    color: #42b983;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+import App from './App.vue';
+import Router from './router';
+
+Vue.use(VueResource);
+
+const application = Vue.extend({
+  components: { App },
+
+  data() {
+    return { pageTitle: null };
+  },
+
+  events: {
+    onPageTitleChanged: function event(title) {
+      this.pageTitle = title;
+    },
+  },
+});
+
+Router.start(application, 'html');
